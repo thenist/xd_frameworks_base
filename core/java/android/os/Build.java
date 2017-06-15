30dca467e3907bdb85da4aa01216957ecba4016a
Daniel Micay <danielmicay@gmail.com>
2017-06-15 19:35:32 -0400

    Radio: Enable boot/bootloader/radio version checks
    This enables the Build.isBuildConsistent checks after tweaking the radio
    check to stop failing when the radio hasn't been turned on. The property
    used to check the radio version (gsm.baseband.version) is only set once
    the radio is turned on and initialized.
    
    The API documentation already claims that this is done.

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -1262,7 +1262,6 @@ public class Build {
             }
         }
 
-        /* TODO: Figure out issue with checks failing
         if (!TextUtils.isEmpty(bootimage)) {
             if (!Objects.equals(system, bootimage)) {
                 Slog.e(TAG, "Mismatched fingerprints; system reported " + system
@@ -1279,14 +1278,13 @@ public class Build {
             }
         }
 
-        if (!TextUtils.isEmpty(requiredRadio)) {
+        if (!TextUtils.isEmpty(requiredRadio) && !TextUtils.isEmpty(currentRadio)) {
             if (!Objects.equals(currentRadio, requiredRadio)) {
                 Slog.e(TAG, "Mismatched radio version: build requires " + requiredRadio
                         + " but runtime reports " + currentRadio);
                 return false;
             }
         }
-        */
 
         return true;
     }
